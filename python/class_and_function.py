d5f589008c9f94f708dba575e74885e143bb9e1d
auroraustc <fanyi@mail.ustc.edu.cn>
2019-04-08 15:34:14 +0800

    read data--still slow, solve later

--- python/class_and_function.py
+++ python/class_and_function.py
@@ -1,5 +1,8 @@
 import numpy as np
 import torch as tf
+import os
+import sys
+import termios
 
 class Parameters():
     def __init__(self):
@@ -13,6 +16,33 @@ class Parameters():
         Nframes_tot = 0
         sym_coord_type = 1
 
+
+def press_any_key_exit(msg):
+    # 获取标准输入的描述符
+    fd = sys.stdin.fileno()
+
+    # 获取标准输入(终端)的设置
+    old_ttyinfo = termios.tcgetattr(fd)
+
+    # 配置终端
+    new_ttyinfo = old_ttyinfo[:]
+
+    # 使用非规范模式(索引3是c_lflag 也就是本地模式)
+    new_ttyinfo[3] &= ~termios.ICANON
+    # 关闭回显(输入不会被显示)
+    new_ttyinfo[3] &= ~termios.ECHO
+
+    # 输出信息
+    sys.stdout.write(msg)
+    sys.stdout.flush()
+    # 使设置生效
+    termios.tcsetattr(fd, termios.TCSANOW, new_ttyinfo)
+    # 从终端读取
+    os.read(fd, 7)
+
+    # 还原终端设置
+    termios.tcsetattr(fd, termios.TCSANOW, old_ttyinfo)
+
 def read_parameters(parameters):
     if (type(parameters).__name__ != "Parameters"):
         print("type error in read_parameters:", type(parameters).__name__," is NOT a correct Parameters class")
@@ -23,7 +53,7 @@ def read_parameters(parameters):
     parameters.N_types_all_frame = 2
     parameters.type_index_all_frame = [0, 1]
     parameters.SEL_A_max = 200
-    parameters.Nframes_tot = 1299
+    parameters.Nframes_tot = 120
     parameters.sym_coord_type = 1
     return 0
 
@@ -33,7 +63,7 @@ def reshape_to_frame_wise(source, n_atoms, parameters, flag):
         return 1
     if (flag == 1):#force and coord
         interval = 3
-    elif (flag == 2):#sym_coord
+    elif (flag == 2):#DeePMD type sym_coord
         interval = parameters.SEL_A_max * 4
     start_atom = 0
     end_atom = n_atoms[0] - 1
@@ -44,20 +74,40 @@ def reshape_to_frame_wise(source, n_atoms, parameters, flag):
         source_Reshape = np.append(source_Reshape,
                                   [np.append([], [source[j] for j in range(start_atom * interval, (end_atom + 1) * interval)])],
                                   axis=0)
+        """if (i == 5): #For debug and compare
+            print(source_Reshape)
+            break"""
     return source_Reshape
 
-###not complete
-def read_reshape(target, dtype, n_atoms, parameters, flag):
+###Allso slow. Not used
+def read_reshape_DeepMD(n_atoms, parameters):
     if (type(parameters).__name__ != "Parameters"):
         print("type error in reshape:", type(parameters).__name__," is NOT a correct Parameters class")
         return 1
     fp = open("./SYM_COORD.BIN", "rb")
+    interval_per_atom = parameters.SEL_A_max * 4 * np.dtype(np.float64).itemsize
     start_atom = 0
     end_atom = n_atoms[0] - 1
-    target = []
-    if (flag == 3):
-        interval = parameters.SEL_A_max * 4
-    byte_offset = end_atom * np.dtype(dtype).itemsize * interval
-
-
+    fp.seek(interval_per_atom * start_atom, os.SEEK_SET)
+    tmp = fp.read(interval_per_atom * (end_atom - start_atom + 1))
+    fp_out = open("./tmp", "wb")
+    fp_out.write(tmp)
+    fp_out.close()
+    tmp_in = np.fromfile("./tmp", dtype = np.float64)
+    source_Reshape = np.array([np.append([], tmp_in)])
+    #print(source_Reshape)
+    for i in range(1, parameters.Nframes_tot):
+        start_atom = end_atom + 1
+        end_atom = end_atom + n_atoms[i]
+        fp.seek(interval_per_atom * start_atom, os.SEEK_SET)
+        tmp = fp.read(interval_per_atom * (end_atom - start_atom + 1))
+        fp_out = open("./tmp", "wb")
+        fp_out.write(tmp)
+        fp_out.close()
+        tmp_in = np.fromfile("./tmp", dtype=np.float64)
+        source_Reshape = np.append(source_Reshape, np.array([np.append([], tmp_in)]), axis = 0)
+        """if (i == 5): #For debug and compare
+            print(source_Reshape)
+            break"""
     fp.close()
+    return source_Reshape
--- python/train.py
+++ python/train.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
 import numpy as np
 import torch as tf
 import os
+import gc
 
 from class_and_function import *
 
@@ -9,7 +13,7 @@ from class_and_function import *
 parameters = Parameters()
 read_parameters_flag = read_parameters(parameters)
 if (read_parameters_flag != 0):
-    print("Reading parameters error with code %d"%read_parameters_flag)
+    print("Reading parameters error with code %d\n"%read_parameters_flag)
     exit()
 
 COORD = np.fromfile("./COORD.BIN", dtype = np.float64)
@@ -20,24 +24,23 @@ TYPE = np.fromfile("./TYPE.BIN", dtype = np.int32)
 N_ATOMS = np.fromfile("./N_ATOMS.BIN", dtype = np.int32)
 print(N_ATOMS)
 print(np.dtype(np.float64).itemsize)
+press_any_key_exit("Read complete. Press any key to continue\n")
 """Reshape COORD, FORCE as [Nfrmaes, N_Atoms_this_frame * 3] and SYM_COORD as [Nframes, N_Atoms_this_frame * SELA_max * 4]"""
 """DO NOT use np.reshape because it cannot deal with frames with different number of atoms."""
 COORD_Reshape = reshape_to_frame_wise(COORD, N_ATOMS, parameters, 1)
 print("COORD_Reshape: \n", COORD_Reshape)
 FORCE_Reshape = reshape_to_frame_wise(FORCE, N_ATOMS, parameters, 1)
 print("FORCE_Reshape: \n", FORCE_Reshape)
-#SYM_COORD_Reshape = reshape_to_frame_wise(SYM_COORD, N_ATOMS, parameters, 2)
-#print("SYM_COORD_Reshape: \n", SYM_COORD_Reshape)
+SYM_COORD_Reshape = reshape_to_frame_wise(SYM_COORD, N_ATOMS, parameters, 2)
+print("SYM_COORD_Reshape: \n", SYM_COORD_Reshape)
 
-###not complete_test
-fp = open("./SYM_COORD.BIN", "rb")
-interval = parameters.SEL_A_max * 4 * 1 * np.dtype(np.float64).itemsize
-fp.seek(interval * (N_ATOMS[0] * 5 + 2), os.SEEK_SET)
-tmp = fp.read(parameters.SEL_A_max * 4 * 1 * np.dtype(np.float64).itemsize)
-fp_out = open("./tmp", "wb")
-fp_out.write(tmp)
-tmp = np.fromfile("./tmp", dtype = np.float64)
-tmp1 = np.array([np.append([], tmp)])
-print(tmp1)
-fp_out.close()
-fp.close()
+"""SYM_COORD_Reshape_2 = read_reshape_DeepMD(N_ATOMS, parameters)
+print("2:")
+print(SYM_COORD_Reshape_2)"""
+
+press_any_key_exit("Press any key to free memory.\n")
+del COORD
+del FORCE
+del SYM_COORD
+gc.collect()
+press_any_key_exit("Memory free complete.\n")

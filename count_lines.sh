cbc3f61685cff98ce99cd7ce77e9fafa462f71d0
auroraustc <fanyi@mail.ustc.edu.cn>
2019-06-22 18:45:26 +0800

    compute descriptors and derivatives for DPMD on GPU

--- count_lines.sh
+++ count_lines.sh
@@ -1,1 +1,1 @@
-for i in ./python/no_mpi/*.py ./c/*.[c,h]*; do echo $i; done | xargs wc -l
+for i in ./python/no_mpi/*.py ./python/no_mpi/*.c* ./c/*.[c,h]* ./c/train_nompi/*.[c,h]*; do echo $i; done | xargs wc -l
